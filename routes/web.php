0b5e1746ab5c770c3a3cc5ae261dc5fafcb91a66
sytnic <sytnic@list.ru>
2021-12-10 13:39:55 +0300

    7. Make a Route and Link to it

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+       return view('posts');
+    // return view('welcome');
     // return [ "foo" => "bar"];
     // return 'Hello world';
+});
+
+Route::get('post', function () {
+    return view('post');
 });
