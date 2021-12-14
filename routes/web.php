aa511a8253f795e2f5eeeef3f459d76e39a7d4e2
sytnic <sytnic@list.ru>
2021-12-14 11:29:52 +0300

    10. Use Caching for Expensive Operations

--- routes/web.php
+++ routes/web.php
@@ -25,18 +25,29 @@ Route::get('posts/{post}', function ($slug) {
 
     $path = __DIR__."/../resources/posts/{$slug}.html";
 
-    if(! file_exists($path)){
+    if(! file_exists($path)) {
         //ddd("file does not exist");
         //abort(404);
         return redirect('/');
     }
 
-    $post = file_get_contents($path);
+    // кэширование контента для get-запроса
+    // в секундах (integer) или с помощью функции now()->addMinutes(20)
+    
+    $post = cache()->remember("posts.{$slug}", 1200, function() use($path) {
+        var_dump('file_get_contents');
+        return file_get_contents($path);
+      }
+    );  
+    
+    // короткая версия    
+    //$post = cache()->remember("posts.{$slug}", 5, fn() => file_get_contents($path));
 
     return view('post', [
             'post' => $post
             //'post' => '<h3>Hello world</h3>' // $post
         ]        
     );
-
+// чтобы только такие посты отрабатывали в адресной строке, 
+// с остальными символами - 404
 })->where('post', '[A-z_\-]+')  ;  // whereAlpha('post');
