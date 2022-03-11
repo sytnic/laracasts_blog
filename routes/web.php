50e341bd6f31cffec83027ba7c2dda3b0bbceda0
sytnic <sytnic@list.ru>
2022-03-11 12:00:52 +0300

    8.4 if slug is wrong, final version.

--- routes/web.php
+++ routes/web.php
@@ -22,33 +22,26 @@ Route::get('/', function () {
 });
 
 /* Окончательная версия
-Route::get('posts/{post}', function ($slug) {    
-
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-
-    if(! file_exists($path)){
-        //ddd("file does not exist");
-        //abort(404);
-        return redirect('/');
-    }
-
-    $post = file_get_contents($path);
-
-    return view('post', [
-            'post' => $post
-            //'post' => '<h3>Hello world</h3>' // $post
-        ]
-    );
-
-});
 */
-// Предварительная версия 3
+
 // $slug передаётся в file_get_contents и $post
 // работает так: /posts/my-first-post
 Route::get('posts/{post}', function($slug) {         // какой get использовать
        
-    $post = file_get_contents(__DIR__."/../resources/posts/{$slug}.html"); // что значит переменная $post
-    
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+
+    // если путь в слаге указан неверно     
+    if(! file_exists($path)){
+        // Варианты использования:
+
+        // dd("file does not exist");
+        // ddd("file does not exist");
+        // abort(404);
+        return redirect('/');
+    }
+
+    $post = file_get_contents($path); // что значит переменная $post    
+
     return view('post', [               // какой view загружать
         'post' => $post                 // передана переменная $post в этот view
         //'post' => '<h3>Hello world</h3>'
